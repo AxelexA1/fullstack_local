539500e8b8080107c887a0b8b490d76e5eaadd9b
AxelexA1 <71548579+AxelexA1@users.noreply.github.com>
2022-06-22 18:33:14 +0300

    Korjattiin sovelluksen kaatava virhe index.js rivillä 107

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.delete('/api/persons/:id', (req, res) => {
   }
 })
   
-const PORT = 3002
+const PORT = process.env.PORT | 3002
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
